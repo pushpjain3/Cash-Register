8af43de0482f4ae5342ef5f0b47ec3dbd5cb8905
Pushp Jain <pushp.ug20@nsut.ac.in>
2021-08-02 09:00:26 +0530

    logic created

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
 const billInput = document.querySelector("#bill-input");
 const cashInput = document.querySelector("#cash-input");
 const submitButton = document.querySelector("#submit-button");
+const errMsg = document.querySelector("#error");
+const changeLog = document.querySelector("#change-register");
 
-submitButton.addEventListener("click", submitClickHandler)
+const changeAvl = [2000, 500, 200, 100, 50, 20, 10, 5, 2, 1];
 
-const submitClickHandler =()=>{
-    var billAmount = billInput.value;
-    var cashAmount = cashInput.value;
-}
+const calculateChange = (cash, bill) => {
+	var changeRequired = cash - bill;
+	changeAvl.forEach((change, i) => {
+		var changeValue;
+		changeValue = Math.trunc(changeRequired / change);
+		changeLog.rows[1].cells[i + 1].innerText = String(changeValue);
+		changeRequired = changeRequired % change;
+	});
+};
+
+const submitClickHandler = () => {
+	var billAmount = billInput.value;
+	var cashAmount = cashInput.value;
+	if (!isNaN(cashAmount) && !isNaN(billAmount)) {
+		var cash = Number(cashAmount);
+		var bill = Number(billAmount);
+		if (cash >= bill) {
+			calculateChange(cash, bill);
+		} else {
+			errMsg.innerText = "Do you wanna wash dishes?";
+		}
+	} else {
+		errMsg.innerText = "Please enter cash in digits!!";
+	}
+};
+
+submitButton.addEventListener("click", submitClickHandler);
