0305c21122732f3a77481cc15d58f84170090a0b
Pushp Jain <77234511+pushpjain3@users.noreply.github.com>
2021-09-14 02:06:45 +0530

    fixed a bug

--- app.js
+++ app.js
@@ -7,36 +7,41 @@ const changeLog = document.querySelector("#change-register");
 const changeAvl = [2000, 500, 200, 100, 50, 20, 10, 5, 2, 1];
 
 const calculateChange = (cash, bill) => {
-	var changeRequired = cash - bill;
-	changeAvl.forEach((change, i) => {
-		var changeValue;
-		changeValue = Math.trunc(changeRequired / change);
-		changeLog.rows[1].cells[i + 1].innerText = String(changeValue);
-		changeRequired = changeRequired % change;
-	});
+  var changeRequired = cash - bill;
+  changeAvl.forEach((change, i) => {
+    var changeValue;
+    changeValue = Math.trunc(changeRequired / change);
+    changeLog.rows[1].cells[i + 1].innerText = String(changeValue);
+    changeRequired = changeRequired % change;
+  });
 };
 
 const submitClickHandler = () => {
-	var billAmount = billInput.value;
-	var cashAmount = cashInput.value;
-	if (!isNaN(cashAmount) && !isNaN(billAmount)) {
-		var cash = Number(cashAmount);
-		var bill = Number(billAmount);
-		if (cash >= bill) {
-			errMsg.innerText = "";
-			calculateChange(cash, bill);
-		} else {
-			errMsg.innerText = "!! Do you wanna wash dishes?";
-			for (let i = 1; i <= changeAvl.length; i++) {
-				changeLog.rows[1].cells[i].innerText = "";
-			}
-		}
-	} else {
-		errMsg.innerText = "!! Please enter amount in digits !!";
-		for (let i = 1; i <= changeAvl.length; i++) {
-			changeLog.rows[1].cells[i].innerText = "";
-		}
-	}
+  var billAmount = billInput.value;
+  var cashAmount = cashInput.value;
+  if (
+    !isNaN(cashAmount) &&
+    !isNaN(billAmount) &&
+    billAmount[0] !== "-" &&
+    cashAmount !== "-"
+  ) {
+    var cash = Number(cashAmount);
+    var bill = Number(billAmount);
+    if (cash >= bill) {
+      errMsg.innerText = "";
+      calculateChange(cash, bill);
+    } else {
+      errMsg.innerText = "!! Do you wanna wash dishes?";
+      for (let i = 1; i <= changeAvl.length; i++) {
+        changeLog.rows[1].cells[i].innerText = "";
+      }
+    }
+  } else {
+    errMsg.innerText = "!! Please enter amount in valid digits !!";
+    for (let i = 1; i <= changeAvl.length; i++) {
+      changeLog.rows[1].cells[i].innerText = "";
+    }
+  }
 };
 
 submitButton.addEventListener("click", submitClickHandler);
